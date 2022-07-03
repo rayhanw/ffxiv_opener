4ba3b811ed7037df274827bea9f2ffa6aeea268b
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2022-07-03 15:55:30 +0700

    filter out PvP actions

--- api_links.md
+++ api_links.md
@@ -1,25 +1,32 @@
 # API Links:
 ### Tanks
-WAR: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=21;3,ClassJobCategory.WAR=1&sort_field=ClassJobLevel
-GNB: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=37;3,ClassJobCategory.GNB=1&sort_field=ClassJobLevel
-DRK: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=32;3,ClassJobCategory.DRK=1&sort_field=ClassJobLevel
-PLD: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=19;3,ClassJobCategory.PLD=1&sort_field=ClassJobLevel
+WAR: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=21;3,ClassJobCategory.WAR=1
+GNB: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=37;3,ClassJobCategory.GNB=1
+DRK: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=32;3,ClassJobCategory.DRK=1
+PLD: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=19;3,ClassJobCategory.PLD=1
 ### Healers
-WHM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=24;3,ClassJobCategory.WHM=1&sort_field=ClassJobLevel
-SCH: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=28;3,ClassJobCategory.SCH=1&sort_field=ClassJobLevel
-AST: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=33;3,ClassJobCategory.AST=1&sort_field=ClassJobLevel
-SGE: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=40;3,ClassJobCategory.SGE=1&sort_field=ClassJobLevel
+WHM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=24;3,ClassJobCategory.WHM=1
+SCH: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=28;3,ClassJobCategory.SCH=1
+AST: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=33;3,ClassJobCategory.AST=1
+SGE: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=40;3,ClassJobCategory.SGE=1
 ### Melees
-MNK: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=20;2,ClassJobCategory.MNK=1&sort_field=ClassJobLevel
-SAM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=34;2,ClassJobCategory.SAM=1&sort_field=ClassJobLevel
-RPR: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=39;2,ClassJobCategory.RPR=1&sort_field=ClassJobLevel
-DRG: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=22;2,ClassJobCategory.DRG=1&sort_field=ClassJobLevel
-NIN: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=30;2,ClassJobCategory.NIN=1&sort_field=ClassJobLevel
+MNK: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=20;2,ClassJobCategory.MNK=1
+SAM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=34;2,ClassJobCategory.SAM=1
+RPR: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=39;2,ClassJobCategory.RPR=1
+DRG: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=22;2,ClassJobCategory.DRG=1
+NIN: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=30;2,ClassJobCategory.NIN=1
 ### PRanges
-MCH: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=31,ClassJobCategory.MCH=1&sort_field=ClassJobLevel
-DNC: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=38,ClassJobCategory.DNC=1&sort_field=ClassJobLevel
-BRD: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=23;5,ClassJobCategory.BRD=1&sort_field=ClassJobLevel
+MCH: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=31,ClassJobCategory.MCH=1
+DNC: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=38,ClassJobCategory.DNC=1
+BRD: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=23;5,ClassJobCategory.BRD=1
 ### MRanges
-BLM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=25;7,ClassJobCategory.BLM=1&sort_field=ClassJobLevel
-RDM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=35,ClassJobCategory.RDM=1&sort_field=ClassJobLevel
-SMN: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=27;3,ClassJobCategory.SMN=1&sort_field=ClassJobLevel
+BLM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=25;7,ClassJobCategory.BLM=1
+RDM: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=35,ClassJobCategory.RDM=1
+SMN: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=27;3,ClassJobCategory.SMN=1
+
+#### Additional filters/params:
+- IsPvP=0 -> Filter
+- sort_field=ClassJobLevel
+
+## Full Example:
+WAR: https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=21;3,ClassJobCategory.WAR=1,IsPvP=0&sort_field=ClassJobLevel
--- lib/utils.dart
+++ lib/utils.dart
@@ -40,43 +40,43 @@ extension ParseToString on Job {
 String getJobActions(Job job) {
   switch (job) {
     case Job.war:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=21;3,ClassJobCategory.WAR=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=21;3,ClassJobCategory.WAR=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.gnb:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=37;3,ClassJobCategory.GNB=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=37;3,ClassJobCategory.GNB=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.drk:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=32;3,ClassJobCategory.DRK=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=32;3,ClassJobCategory.DRK=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.pld:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=19;3,ClassJobCategory.PLD=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=19;3,ClassJobCategory.PLD=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.whm:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=24;3,ClassJobCategory.WHM=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=24;3,ClassJobCategory.WHM=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.sch:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=28;3,ClassJobCategory.SCH=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=28;3,ClassJobCategory.SCH=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.ast:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=33;3,ClassJobCategory.AST=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=33;3,ClassJobCategory.AST=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.sge:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=40;3,ClassJobCategory.SGE=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=40;3,ClassJobCategory.SGE=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.mnk:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=20;2,ClassJobCategory.MNK=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=20;2,ClassJobCategory.MNK=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.sam:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=34;2,ClassJobCategory.SAM=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=34;2,ClassJobCategory.SAM=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.rpr:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=39;2,ClassJobCategory.RPR=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=39;2,ClassJobCategory.RPR=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.drg:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=22;2,ClassJobCategory.DRG=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=22;2,ClassJobCategory.DRG=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.nin:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=30;2,ClassJobCategory.NIN=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=30;2,ClassJobCategory.NIN=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.mch:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=31,ClassJobCategory.MCH=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=31,ClassJobCategory.MCH=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.dnc:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=38,ClassJobCategory.DNC=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=38,ClassJobCategory.DNC=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.brd:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=23;5,ClassJobCategory.BRD=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=23;5,ClassJobCategory.BRD=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.blm:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=25;7,ClassJobCategory.BLM=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=25;7,ClassJobCategory.BLM=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.rdm:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=35,ClassJobCategory.RDM=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=35,ClassJobCategory.RDM=1,IsPvP=0&sort_field=ClassJobLevel';
     case Job.smn:
-      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=27;3,ClassJobCategory.SMN=1&sort_field=ClassJobLevel';
+      return 'https://xivapi.com/search?indexes=Action&filters=ClassJobTargetID|=27;3,ClassJobCategory.SMN=1,IsPvP=0&sort_field=ClassJobLevel';
   }
 }
 
